2b0c209de1c5461087021c0742275e43efe2f06d
andri090800 <andri.mulyana9f@gmail.com>
2026-05-03 23:25:44 +0700

    Update Gallery.jsx

--- src/components/sections/Gallery.jsx
+++ src/components/sections/Gallery.jsx
@@ -1,4 +1,10 @@
 import StarBackground from "../ui/StarBackground";
+import gallery1 from '../../assets/gallery1.jpg';
+import gallery2 from '../../assets/gallery2.jpg';
+import gallery3 from '../../assets/gallery3.jpg';
+import gallery4 from '../../assets/gallery4.jpg';
+import gallery5 from '../../assets/gallery5.jpg';
+import gallery6 from '../../assets/gallery6.jpg';
 
 export default function Gallery() {
   const images = [
